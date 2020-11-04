101b8217360c507f63757e755c67c26ba2d70697
AlcatrazALT <olegmazalevsky21@yandex.ru>
2020-11-04 14:51:13 +0700

    Destroy enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var bird = collision.collider.GetComponent<GreenBird>();
+        if (bird != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        var enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
